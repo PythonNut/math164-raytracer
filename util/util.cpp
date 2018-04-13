dbb1a43f9f64d00555908054800382597fcd66d4
PythonNut <PythonNut@users.noreply.github.com>
2018-04-13 03:11:53 -0700

    Replace sketchy "default" RNG with Xoroshiro128+

--- util/util.cpp
+++ util/util.cpp
@@ -19,10 +19,33 @@ int toPPM(const double x) {
      return pow(clamp_intensity(x), 1/2.2) * 255 + 0.5;
 }
 
-Random::Random() : uniform_rand(0, 1) {
-     random_device rand_dev;
-     this->rand_engine = default_random_engine(rand_dev());
+Xoroshiro128::Xoroshiro128() {
+     // http://www.iro.umontreal.ca/~lecuyer/myftp/papers/lfsr04.pdf
+     mt19937_64 rng(random_device{}());
+     rng.discard(700000);
+     this->state[0] = rng();
+     this->state[1] = rng();
 }
+
+Xoroshiro128::result_type Xoroshiro128::max() {
+     return numeric_limits<Xoroshiro128::result_type>::max();
+}
+
+Xoroshiro128::result_type Xoroshiro128::min() {
+     return numeric_limits<Xoroshiro128::result_type>::min();
+}
+
+Xoroshiro128::result_type Xoroshiro128::operator()() {
+     uint64_t s0 = this->state[0];
+     uint64_t s1 = this->state[1];
+     uint64_t result = s0 + s1;
+     s1 ^= s0;
+     this->state[0] = ((s0 << 55) | (s0 >> 9)) ^ s1 ^ (s1 << 14);
+     this->state[1] = (s1 << 36) | (s1 >> 28);
+     return result;
+}
+
+Random::Random() : rand_engine(), uniform_rand(0, 1) { }
 
 double Random::unit_rand() {
      return this->uniform_rand(this->rand_engine);
--- util/util.h
+++ util/util.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <random>
+#include <array>
 #include "linear_algebra.h"
 
 using namespace std;
@@ -12,8 +13,22 @@ template <typename T> int sgn(T val);
 
 int toPPM(const double x);
 
+class Xoroshiro128 {
+private:
+     array<uint64_t, 2> state;
+
+public:
+     using result_type = uint64_t;
+
+     Xoroshiro128();
+
+     result_type max();
+     result_type min();
+     result_type operator()();
+};
+
 class Random {
-     default_random_engine rand_engine;
+     Xoroshiro128 rand_engine;
      uniform_real_distribution<> uniform_rand;
 public:
      Random();
