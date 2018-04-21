e3d19915a7072517cfd1a4452ae69b6e6e3992f5
PythonNut <PythonNut@users.noreply.github.com>
2018-04-21 04:15:15 -0700

    Fix progressive rendering convergence

--- raytracer_ppm2.cpp
+++ raytracer_ppm2.cpp
@@ -27,51 +27,39 @@ int main (int argc, char *argv[])
      typedef Matrix<Color, Dynamic, Dynamic> Matrix;
      Matrix m(height, width);
 
-
-     // This is a pretty straightforward port from smallpt
-     // TODO: Make this less horrible.
-for (int s=0; s<samples; s++) {
-#pragma omp parallel for schedule(dynamic) private(rand)
-     for (int y=0; y<height; y++) {
-#pragma omp critical
-          cout << "\rRendering (" << samples << " spp) " << 100.*y/(height-1) << "%" << flush;
-          for (int x=0; x<width; x++) {
-            //int i=(height-y-1)*width+x;
-               Color r(0, 0, 0);
-               //for (int s=0; s<samples; s++) {
+     for (int s=0; s<samples; s++) {
+          cout << "\rRendering (" << samples << "%) " << 100.0*(s+1)/samples << "%" << flush;
+          #pragma omp parallel for schedule(dynamic) private(rand)
+          for (int y=0; y<height; y++) {
+               for (int x=0; x<width; x++) {
                     double dx, dy;
                     tie(dx, dy) = sampler.sample();
                     Vector3d d = cx*((dx/2 + x)/width - .5) + cy*((dy/2 + y)/height - .5) + camera.direction;
-                    d = d.normalized();
+                    d.normalize();
                     Ray ray(camera.origin + d*140, d);
-                    r += radiance(scene, ray, 0, rand)/s;
-               //} // Camera rays are pushed ^^^^^ forward to start in interior
-               //c[i] += clamp_intensity(r)*.25;
-               m(height-y-1, x) += clamp_intensity(r);
+                    Color r = radiance(scene, ray, 0, rand);
+                    m(height-y-1, x) += r;
+               }
           }
+          // ofstream out_file;
+          // out_file.open("imagePPM1.ppm");
+          // out_file << "P3" << endl << width << " " << height << endl << 255 << endl;
+          // for (int j=0; j<height; j++){
+          //      for (int i=0; i<width; i++) {
+          //           out_file << toPPM((m(j,i)).x()) << " " << toPPM((m(j,i)).y()) << " " << toPPM((m(j,i)).z()) << " ";
+          //      }
+          // }
+          // out_file.close();
      }
+
      ofstream out_file;
-     out_file.open("imagePPM1.ppm");
+     out_file.open("imageDsP.ppm");
      out_file << "P3" << endl << width << " " << height << endl << 255 << endl;
      for (int j=0; j<height; j++){
-        for (int i=0; i<width; i++) {
-          out_file << toPPM((m(j,i)).x()) << " " << toPPM((m(j,i)).y()) << " " << toPPM((m(j,i)).z()) << " ";
-        }
+          for (int i=0; i<width; i++) {
+               out_file << toPPM((m(j,i)/samples).x()) << " " << toPPM((m(j,i)/samples).y()) << " " << toPPM((m(j,i)/samples).z()) << " ";
+          }
      }
      out_file.close();
-
-}
-/*
-ofstream out_file;
-out_file.open("imageDsP.ppm");
-out_file << "P3" << endl << width << " " << height << endl << 255 << endl;
-for (int j=0; j<height; j++){
-   for (int i=0; i<width; i++) {
-     out_file << toPPM((m(j,i)).x()) << " " << toPPM((m(j,i)).y()) << " " << toPPM((m(j,i)).z()) << " ";
-   }
-}
-out_file.close();
-*/
-
-
+     return 0;
 }
